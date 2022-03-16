ceebd02d16c7b8d5150ecd062e864be04de1212c
Yves Schär <yves.schaer@gmail.com>
2022-03-16 17:39:36 +0100

    firebase

---
+++ lib/utils/player.dart
@@ -0,0 +1,13 @@
+class Player {
+  final int id;
+  final String firstName;
+  final String lastName;
+  final DateTime birthday;
+
+  Player({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.birthday,
+  });
+}
